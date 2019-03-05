a733df12334beaecaaf509d8b2dc815104326606
Robin Hofman <roobin1999@gmail.com>
2019-03-05 18:14:18 +0100

    id gets added to 'a'

--- src/js/app.js
+++ src/js/app.js
@@ -1,71 +1,116 @@
-
 import {API} from "./index.js";
 
-(function() {
+localStorage.clear();
 
 console.log("Succesfully imported: app.js");
 
-function addToDocument(response) {  
-    console.log(response)
-
-    var books = document.getElementById('books');
-    var loader = document.getElementById('loader');
-    loader.setAttribute('style', 'display: none;');
-
-    for(var i=0; i<response.length ; i++) {
-        var book = document.createElement('div');
-        book.setAttribute('class', 'book');
-
-        var title = document.createElement('h2');
-        // title = title.split('/');
-        title.setAttribute('class', 'title');
-        title.innerHTML = response[i]['title']['full'].split('/')[0];
-
-        var cover = document.createElement('img');
-        cover.setAttribute('class', 'cover');
-        var image = response[i]['images'][0];
-        cover.setAttribute('src', image);
-
-        var desc = document.createElement('p');
-        desc.setAttribute('class', 'description');
-        var description = response[i]['summary'];
-        if(typeof description !== 'undefined') {
-            desc.innerHTML = description.slice(0, 200);
-        } else {
-            desc.innerHTML = "Description not available";
-        }
-
-        book.appendChild(title);
-        book.appendChild(cover);
-        book.appendChild(desc);
-
-        books.appendChild(book);
-    }
+//-----------SET DEFAULT HASH TO "HOME"-----------//
+if(window.location.hash === "") {
+    window.location.hash = "home";
 }
 
-(async () => {
-    localStorage.clear();
-    
-	const api = new API({
-		key: "1e19898c87464e239192c8bfe422f280"
+(async() => {
+
+    const api = new API({
+        key: "1e19898c87464e239192c8bfe422f280"
     });
     
-    async function searchBook(name) {
-        var loader = document.getElementById("loader");
-        loader.setAttribute('style', 'display: block;');
+    const wrapper = {
+        searchBook: async function(name) {
+            var loader = document.getElementById("loader");
+            loader.setAttribute('style', 'display: block;');
 
-        console.log("Searching Book: " + name + "...");
-        
-        const stream = await api.createStream("search/" + name + "{9}");
+            console.log("Searching Book: " + name + "...");
+            
+            const stream = await api.createStream("search/" + name + "{9}");
 
-        stream
-            .pipe(addToDocument)
-            .catch(console.error);
+            stream
+                .pipe(render.addToDocument)
+                .catch(console.error);
+        }
     }
-        
+
+    const renderer = {
+        addToDocument: function(response) {
+            console.log(response)
+
+            var books = document.getElementById('books');
+            var loader = document.getElementById('loader');
+            loader.setAttribute('style', 'display: none;');
+    
+            for(var i=0; i<response.length ; i++) {
+
+                var clickable = document.createElement('a');
+                var identifiers = response[i]['identifiers'];
+
+                for(var x=0; x<identifiers.length; x++) {   
+                    var temp = identifiers[x]['ppn-id'];
+                    if(typeof temp !== 'undefined') {
+                        console.log("gevonden: " + identifiers[x]['ppn-id']);
+                        var id = identifiers[x]['ppn-id'];
+                    }
+                }  
+
+                clickable.setAttribute('href', '#availability?id=' + id);
+                
+                var book = document.createElement('div');
+                book.setAttribute('class', 'book');
+    
+                var title = document.createElement('h2');
+                // title = title.split('/');
+                title.setAttribute('class', 'title');
+                var titel = response[i]['title']['full'];
+                
+                if(typeof titel === 'undefined') {
+                    titel = "Unknown";
+                } else {
+                    titel = titel.split('/')[0].slice(0,50);
+                }
+                title.innerHTML = titel;
+    
+                var cover = document.createElement('img');
+                cover.setAttribute('class', 'cover');
+                var image = response[i]['images'][0];
+                cover.setAttribute('src', image);
+    
+                var desc = document.createElement('p');
+                desc.setAttribute('class', 'description');
+                var description = response[i]['summary'];
+                if(typeof description !== 'undefined') {
+                    desc.innerHTML = description.slice(0, 200);
+                } else {
+                    desc.innerHTML = "Description not available";
+                }
+    
+                book.appendChild(title);
+                book.appendChild(cover);
+                book.appendChild(desc);
+                
+                clickable.appendChild(book);
+
+                books.appendChild(clickable);
+            }
+        }
+    }
+
+    //-----------VARIABLES-----------//
+    const render = Object.create(renderer);
+    const wrap = Object.create(wrapper);
+
+    //-----------ROUTING-----------//
+    routie({
+		'home': function() {
+			console.log('Home pagina!');
+		},
+		'availability/?:id': function(id) {
+			id = id.substr(4) // '?:id=8892374' -> '8892374'
+			// route.pokemonID(id);
+		}
+    });
+    
+    //-----------EVENT LISTENERS-----------//
     document.getElementById("search").addEventListener("click", function(){
         name = document.getElementById("input").value;
-        searchBook(name);
+        wrap.searchBook(name);
     });
-})()
 })();   
